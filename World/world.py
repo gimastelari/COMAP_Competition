c7d877f6aed5e8f871593aa2e936f33da2f769eb
gimastelari <gimastelari@gmail.com>
2025-01-25 16:03:31 -0500

    importing new dataset (fixed_broadband_subscriptions)

--- World/world.py
+++ World/world.py
@@ -1,2 +1,7 @@
 individuals_using_internet = open("World/individuals_using_internet.csv", "r")
 print(individuals_using_internet.read(), "\n")
+
+fixed_broadband_subscriptions= open("World/fixed_broadband_subscriptions.csv", "r")
+print(fixed_broadband_subscriptions.read(), "\n")
+
+
