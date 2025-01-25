340766a4fe9e92353b7248b15ca2a1b4d2fd1235
gimastelari <gimastelari@gmail.com>
2025-01-25 16:54:30 -0500

    Merge branch 'main' of https://github.com/gimastelari/COMAP_Competition

--- World/world.py
+++ World/world.py
@@ -4,4 +4,9 @@ print(individuals_using_internet.read(), "\n")
 fixed_broadband_subscriptions= open("World/fixed_broadband_subscriptions.csv", "r")
 print(fixed_broadband_subscriptions.read(), "\n")
 
+mobile_data_and_voice_low_consumption_baskets = open("World/mobile_data_and_voice_low_consumption_baskets.csv", "r")
+print(mobile_data_and_voice_low_consumption_baskets.read(), "\n")
+
+
+
 
