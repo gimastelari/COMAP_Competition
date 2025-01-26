387639ebe784f7dc63e32bc7f952cd37e7a50186
SanteIsHere <r.a.frye00@gmail.com>
2025-01-25 19:40:14 -0500

    Merge branch 'main' of https://github.com/gimastelari/COMAP_Competition

--- World/world.py
+++ World/world.py
@@ -1,7 +1,31 @@
+
 individuals_using_internet = open("World/individuals_using_internet.csv", "r")
 print(individuals_using_internet.read(), "\n")
 
 fixed_broadband_subscriptions= open("World/fixed_broadband_subscriptions.csv", "r")
 print(fixed_broadband_subscriptions.read(), "\n")
 
+mobile_data_and_voice_low_consumption_baskets = open("World/mobile_data_and_voice_low_consumption_basket.csv", "r")
+print(mobile_data_and_voice_low_consumption_baskets.read(), "\n")
 
+broadband_services_are_part_of_universal_service_access_scheme = open("World/broadband_services_are_part_of_universal_service_access_scheme.csv", "r")
+print(broadband_services_are_part_of_universal_service_access_scheme.read(), "\n")
+
+regulatory_authority_decision_making = open("World/regulatory_authority_decision_making.csv", "r")
+print(regulatory_authority_decision_making.read(), "\n")
+
+level_of_competition = open("World/level_of_competition.csv", "r")
+print(level_of_competition.read(), "\n")
+
+digital_development_strategies = open("World/digital_development_strategies.csv", "r")
+print(digital_development_strategies.read(), "\n")
+
+
+publicly_available_information = open("World/publicly_available_information.csv", "r")
+print(publicly_available_information.read(), "\n")
+
+taxation_policies = open("World/taxation_policies.csv", "r")
+print(taxation_policies.read(), "\n")
+
+cybersecurity_framework_and_mandates = open("World/cybersecurity_framework_and_mandates.csv", "r")
+print(cybersecurity_framework_and_mandates.read(), "\n")
